fa723b0cf3126f56736ea2b792488da3d019199b
Andi <andipabst25@gmail.com>
2022-05-15 13:01:19 +0200

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::future::Future;
+use std::path::PathBuf;
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
@@ -58,85 +59,93 @@ async fn connect(code: Option<String>) -> Result<(String, Wormhole), WormholeErr
     }
 }
 
+struct NoOpFuture {}
+
+impl Future for NoOpFuture {
+    type Output = ();
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        Poll::Pending
+    }
+}
+
 #[wasm_bindgen]
 pub async fn send(file_input: web_sys::HtmlInputElement, output: web_sys::HtmlElement) {
     wasm_logger::init(wasm_logger::Config::default());
     console_error_panic_hook::set_once();
 
+    //Check the file list from the input
+    let filelist = file_input.files().expect("Failed to get filelist from File Input!");
+    //Do not allow blank inputs
+    if filelist.length() < 1 {
+        alert("Please select at least one file.");
+        return;
+    }
+    if filelist.get(0) == None {
+        alert("Please select a valid file");
+        return;
+    }
+
+    let file = filelist.get(0).expect("Failed to get File from filelist!");
+
+    let file_reader: web_sys::FileReader = web_sys::FileReader::new().expect("failed to create a file reader");
+
+    let onloadend_cb = Closure::wrap(Box::new(move |event: web_sys::ProgressEvent| {
+        let file_reader_target = event.target().unwrap().dyn_into::<web_sys::FileReader>().unwrap();
+        let array = js_sys::Uint8Array::new(&file_reader_target.result().unwrap());
+        let len = array.byte_length() as u64;
+        let data_to_send: Vec<u8> = array.to_vec();
+        log(&format!("Raw data ({} bytes): {:?}", len, data_to_send));
+
+        wasm_bindgen_futures::spawn_local(send_via_wormhole(data_to_send, len))
+    }) as Box<dyn Fn(web_sys::ProgressEvent)>);
+
+    file_reader.set_onloadend(Some(onloadend_cb.as_ref().unchecked_ref()));
+    file_reader.read_as_array_buffer(&file).expect("blob not readable");
+    onloadend_cb.forget();
+}
+
+async fn send_via_wormhole(file: Vec<u8>, file_size: u64) {
     match connect(None).await {
         Ok((data, wormhole)) => {
-            output.set_inner_text(&data);
+            //output.set_inner_text(&data);
+            log(&format!("Connected. Code: {}", data));
 
-            //Check the file list from the input
-            let filelist = file_input.files().expect("Failed to get filelist from File Input!");
-            //Do not allow blank inputs
-            if filelist.length() < 1 {
-                alert("Please select at least one file.");
-                return;
-            }
-            if filelist.get(0) == None {
-                alert("Please select a valid file");
-                return;
-            }
+            /*let transfer_result = transfer::send_file(
+                wormhole,
+                url::Url::parse("ws://relay.magic-wormhole.io:4000/v1").unwrap(),
+                &mut &file[..],
+                PathBuf::from("file"),
+                file_size,
+                transit::Abilities::FORCE_RELAY,
+                move |_sent, _total| {
+                    // if sent == 0 {
+                    //     pb2.reset_elapsed();
+                    //     pb2.enable_steady_tick(250);
+                    // }
+                    // pb2.set_position(sent);
+                },
+                |_cur, _total| {},
+                NoOpFuture {},
+            ).await;
+            */
 
-            let file = filelist.get(0).expect("Failed to get File from filelist!");
+            let transfer_result: Result<(), ()> = Ok(());
 
-            let file_reader: web_sys::FileReader = match web_sys::FileReader::new() {
-                Ok(f) => f,
-                Err(e) => {
-                    alert("There was an error creating a file reader");
-                    log(&JsValue::as_string(&e).expect("error converting jsvalue to string."));
-                    web_sys::FileReader::new().expect("")
+            match transfer_result {
+                Ok(_) => {
+                    log(&format!("Data sent"));
                 }
-            };
-
-            let fr_c = file_reader.clone();
-            // create onLoadEnd callback
-            let onloadend_cb = Closure::wrap(Box::new(move |_e: web_sys::ProgressEvent| {
-                let mut array = js_sys::Uint8Array::new(&fr_c.result().unwrap());
-                let len = array.byte_length() as u64;
-                log(&format!("Raw data ({} bytes): {:?}", len, array.to_vec()));
-                // here you can for example use the received image/png data
-
-                struct NoOpFuture {}
-                impl Future for NoOpFuture {
-                    type Output = ();
-
-                    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-                        Poll::Pending
-                    }
+                Err(_) => {
+                    log(&format!("Error in data transfer"));
                 }
-                
-                let relay_url = url::Url::parse("ws://relay.magic-wormhole.io:4000/v1").unwrap();
-
-                let data = array.to_vec();
-                
-                transfer::send_file(
-                    wormhole,
-                    relay_url,
-                    &mut &data[..],
-                    "file".into(),
-                    len,
-                    transit::Abilities::FORCE_RELAY,
-                    move |_sent, _total| {
-                        // if sent == 0 {
-                        //     pb2.reset_elapsed();
-                        //     pb2.enable_steady_tick(250);
-                        // }
-                        // pb2.set_position(sent);
-                    },
-                    NoOpFuture {},
-                )
-            }) as Box<dyn Fn(web_sys::ProgressEvent)>);
-
-            file_reader.set_onloadend(Some(onloadend_cb.as_ref().unchecked_ref()));
-            file_reader.read_as_array_buffer(&file).expect("blob not readable");
-            onloadend_cb.forget();
-        },
-        Err(_) => {
-            output.set_inner_text(&"Error in connection".to_string());
+            }
         }
-    }
+        Err(_) => {
+            //output.set_inner_text(&"Error in connection".to_string());
+            log(&format!("Error in connection"));
+        }
+    };
 }
 
 #[wasm_bindgen]
@@ -147,7 +156,7 @@ pub async fn receive(code: String, output: web_sys::HtmlElement) {
     match connect(Some(code)).await {
         Ok((data, wormhole)) => {
             output.set_inner_text(&data);
-        },
+        }
         Err(_) => {
             output.set_inner_text(&"Error in connection".to_string());
         }
